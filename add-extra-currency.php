ee41eaffae2b4a4f63d9b8a24d07fa0e014e6ad4
ruelporlas <52828902+ruelporlas@users.noreply.github.com>
2019-08-22 19:02:52 +0800

    Adds a custom currency to the list of available currencies on WP Ultimo

--- add-extra-currency.php
+++ add-extra-currency.php
@@ -2,6 +2,9 @@
 
 /**
  * Adds a custom currency to the list of available currencies on WP Ultimo
+ * 
+ * Change the value to variable $extra to whichever currency you would like to add.
+ *
  */
 add_filter('wu_currencies', function($currencies) {
 
