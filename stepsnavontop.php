dcb5f2fe90a4e453d52c13651932369393ccfcdc
ruelporlas <52828902+ruelporlas@users.noreply.github.com>
2019-08-22 19:41:49 +0800

    Move steps navigation to top of the page

--- stepsnavontop.php
+++ stepsnavontop.php
@@ -1,6 +1,10 @@
 /**
- *  Move steps navigation to top of the page
- */
+*  Move steps navigation to top of the page
+*
+* INSTRUCTIONS:
+* This code will simply more the steps navigation bar from the bottom ot the page to the top.
+* You can use the code as is. No changes needed.
+*/
 
 add_action('admin_print_footer_scripts', 'steps_ultimo_top', 10);
 function steps_ultimo_top() { ?>
