9a75f78293273c377e087e2340f8735deebc252b
ruelporlas <52828902+ruelporlas@users.noreply.github.com>
2019-08-22 19:35:52 +0800

    Hide WP Ultimo meta box on admin dashboard

--- hidemetabox.php
+++ hidemetabox.php
@@ -1,13 +1,16 @@
 
 /**
- * Hide WP Ultimo meta box on admin dashboard
- */
+* Hide WP Ultimo meta box on admin dashboard
+*
+* INSTRUCTION:
+* This code will remove WP Ultimo meta boxes from end-user's admin dashboard.
+* Each line represent one metabox. Just remove the line for the metabox you wish to display.
+*
+*/
 
 add_action('admin_head', function() {
-
   remove_meta_box('wp-ultimo-change-plan', 'wu-my-account', 'normal');
   remove_meta_box('wp-ultimo-status', 'wu-my-account', 'normal');
   remove_meta_box('wp-ultimo-actions', 'wu-my-account', 'normal');
   remove_meta_box('wu-mb-billing-history', 'wu-my-account', 'side');
-
 }, 30);
