da9aa93850d238592dafd23c4f975358706bc2e5
ruelporlas <52828902+ruelporlas@users.noreply.github.com>
2019-08-22 19:19:14 +0800

    Adds new user role on sign up

--- adduserrole.php
+++ adduserrole.php
@@ -1,6 +1,14 @@
 /**
- * Add user role on sign up
- */
+* Add user role on sign up
+*
+* INSTRUCTION :
+* This example adds a second user role 'Author' to user account upon registration. 
+* Note: first role or default user role is what you set under WP Ultimo settings.
+*
+* To add a different role other than 'Author', you simply just need to change the value of the code found on line 18 for example.
+* $curr_user->add_role('editor');
+*
+*/
 
 add_action('wp_ultimo_registration', function($site_id, $user_id) {
 
